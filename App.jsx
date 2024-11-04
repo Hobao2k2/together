b88700182c40dcdc021472a7cd6a7a84d046240e
tvanh080502 <tvanh080502@gmail.com>
2024-11-04 20:41:44 +0700

    Ẩn các thông báo lỗi

--- App.jsx
+++ App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/router/appNavigator';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreAllLogs();
 
 export default function App() {
   return (
