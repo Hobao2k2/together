084466f9580a191f3294729596d1f62c40055448
tvanh080502 <tvanh080502@gmail.com>
2024-11-12 19:15:08 +0700

    thêm phần hiển thị thông báo dạng toast cho toàn bộ ứng dụng

--- App.jsx
+++ App.jsx
@@ -1,14 +1,48 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/router/appNavigator';
+import Toast, { BaseToast, ErrorToast } from 'react-native-toast-message'; 
 import { LogBox } from 'react-native';
 
 LogBox.ignoreAllLogs();
+
+// Tùy chỉnh giao diện Toast
+const toastConfig = {
+  success: (props) => (
+    <BaseToast
+      {...props}
+      style={{ borderLeftColor: 'green' }}
+      text1Style={{
+        fontSize: 16,
+        fontWeight: 'bold',
+      }}
+      text2Style={{
+        fontSize: 14,
+        color: '#333',
+      }}
+    />
+  ),
+  error: (props) => (
+    <ErrorToast
+      {...props}
+      text1Style={{
+        fontSize: 16,
+        fontWeight: 'bold',
+      }}
+      text2Style={{
+        fontSize: 14,
+        color: '#333',
+      }}
+    />
+  ),
+};
 
 export default function App() {
   return (
     <NavigationContainer>
       <AppNavigator />
+      {/* Thêm Toast với cấu hình tùy chỉnh */}
+      <Toast config={toastConfig} />
     </NavigationContainer>
   );
 }
